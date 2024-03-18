500ffeecd1e110aaa511280fe34004ec2b6d0f82
Saqibqazi7 <160391573+Saqibqazi7@users.noreply.github.com>
2024-03-19 03:32:39 +0500

    Add files via upload

---
+++ day4.js
@@ -0,0 +1,15 @@
+//Day 4 challage
+//Question no 10
+/*let My_name: string = 'Saqib'
+console.log('Hi', My_name, 'how are you today?');*/
+//Question no 11
+/*let Names: string[] = ['Muhammad', 'Saqib', 'Qazi'];
+for (let i = 0; i < Names.length; i++){
+    console.log(Names[i]);
+}*/
+//Question no 12
+var names = ['Muhammad', 'Saqib', 'Qazi'];
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var name_1 = names_1[_i];
+    console.log('Hello', { name: name_1 }, 'how are you?');
+}
---
+++ day4.ts
@@ -0,0 +1,16 @@
+//Day 4 challage
+//Question no 10
+/*let My_name: string = 'Saqib'
+console.log('Hi', My_name, 'how are you today?');*/
+
+//Question no 11
+/*let Names: string[] = ['Muhammad', 'Saqib', 'Qazi'];
+for (let i = 0; i < Names.length; i++){
+    console.log(Names[i]);
+}*/
+
+//Question no 12
+/*let names: string[] = ['Muhammad','Saqib','Qazi'];
+for (let name of names) {
+    console.log('Hello', {name}, 'how are you?');
+}*/
